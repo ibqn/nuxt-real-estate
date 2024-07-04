53e54fb12baeb81af4980205df1e6a33fad4e125
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-07-04 11:01:27 +0200

    improve header

--- src/components/header.vue
+++ src/components/header.vue
@@ -6,14 +6,15 @@
       </NuxtLink>
 
       <nav class="flex items-center gap-6">
-        <NuxtLink to="/" class="transition hover:text-violet-900"
-          >Sign In</NuxtLink
-        >
+        <NuxtLink to="/" class="transition hover:text-violet-900">
+          Sign In
+        </NuxtLink>
         <NuxtLink
           to="/"
           class="rounded-lg bg-violet-700 px-4 py-3 text-white transition hover:bg-violet-800"
-          >Sign Up</NuxtLink
         >
+          Sign Up
+        </NuxtLink>
       </nav>
     </div>
   </header>
