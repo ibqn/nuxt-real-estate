58e10748279129ff79ce0ee944a09d6917c79540
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-07-04 10:59:38 +0200

    improve footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,1 +1,11 @@
-<template>footer</template>
+<script lang="ts" setup>
+const currentYear = ref(new Date().getFullYear())
+</script>
+
+<template>
+  <footer class="bg-black py-8 text-center text-white">
+    <div class="container mx-auto">
+      Copyright &copy; {{ currentYear }}. All right reserved.
+    </div>
+  </footer>
+</template>
