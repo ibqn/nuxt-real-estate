67645674138540dd3be086148fbb9d4e989cb883
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-07-05 10:57:20 +0200

    format price

---
+++ src/components/price.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { formatPrice } from "@/utils/price-formatter"
+import { cn } from "@/utils/class-names"
+import { type HtmlHTMLAttributes } from "vue"
+
+type Props = {
+  price: number
+}
+
+const props = defineProps<Props & { class?: HtmlHTMLAttributes["class"] }>()
+
+const price = computed(() => formatPrice(props.price))
+</script>
+
+<template>
+  <span :class="cn('text-lg font-semibold text-violet-600', props.class)">{{
+    price
+  }}</span>
+</template>
