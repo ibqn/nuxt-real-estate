53d2a26637467fff747f124ba62ff83a371b76ba
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-07-06 00:10:44 +0200

    improve dropdown styles

--- src/components/country-dropdown.vue
+++ src/components/country-dropdown.vue
@@ -1,5 +1,54 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Dropdown, DropdownContent, DropdownItem } from "@/components/dropdown"
+import { useQuery } from "@tanstack/vue-query"
+import { getCountries } from "@/apis/country"
+import { ChevronUp, MapPin } from "lucide-vue-next"
+import { cn } from "@/utils/class-names"
+
+const defaultLocation = "Location (any)"
+const selectedCountry = ref<string>(defaultLocation)
+
+const { data, suspense } = useQuery({
+  queryKey: ["countries"],
+  queryFn: getCountries,
+})
+
+const countries = computed(() => [defaultLocation, ...(data.value ?? [])])
+await suspense()
+</script>
 
 <template>
-  <div>country</div>
+  <Dropdown class="dropdown">
+    <template #toggler="slotProps">
+      <button class="dropdown-btn w-full text-left">
+        <MapPin class="dropdown-icon-primary" />
+
+        <div>
+          <div class="text-[15px] font-medium leading-tight">
+            {{ selectedCountry }}
+          </div>
+          <div class="text-[13px]">Select your place</div>
+        </div>
+
+        <ChevronUp
+          :class="
+            cn(
+              'dropdown-icon-secondary transition',
+              slotProps.active ? 'rotate-0' : 'rotate-180'
+            )
+          "
+        />
+      </button>
+    </template>
+
+    <DropdownContent class="dropdown-content">
+      <DropdownItem
+        v-for="(country, index) in countries"
+        :key="index"
+        class="dropdown-item"
+      >
+        {{ country }}
+      </DropdownItem>
+    </DropdownContent>
+  </Dropdown>
 </template>
--- src/components/search.vue
+++ src/components/search.vue
@@ -3,11 +3,17 @@ import { Search } from "lucide-vue-next"
 import CountryDropdown from "@/components/country-dropdown.vue"
 import PropertyDropdown from "@/components/property-dropdown.vue"
 import PriceRangeDropdown from "@/components/price-range-dropdown.vue"
+import { cn } from "@/utils/class-names"
 </script>
 
 <template>
   <div
-    class="relative mx-auto flex max-w-[1170px] flex-col justify-between gap-4 rounded-lg bg-white px-[30px] py-6 lg:-top-4 lg:flex-row lg:gap-3 lg:bg-transparent lg:shadow-1 lg:backdrop-blur"
+    :class="
+      cn(
+        'relative mx-auto flex max-w-[1170px] flex-col justify-between gap-4 rounded-lg',
+        'bg-white px-[30px] py-6 lg:-top-4 lg:flex-row lg:gap-3 lg:bg-transparent lg:shadow-1 lg:backdrop-blur'
+      )
+    "
   >
     <CountryDropdown />
     <PropertyDropdown />
