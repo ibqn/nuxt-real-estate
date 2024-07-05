45108b464f5159c0dd8eba6de38f93a09b2e2067
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-07-05 17:29:22 +0200

    move query tools from layout to app comp

---
+++ src/app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { VueQueryDevtools } from "@tanstack/vue-query-devtools"
+</script>
+
+<template>
+  <VueQueryDevtools />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
