f5f7c45e5adf4d95d8de0389ae5becdea50463fd
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-07-05 10:57:47 +0200

    design house comp

--- src/components/house.vue
+++ src/components/house.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { type House } from "@/types"
+import { Bath, BedDouble, Scaling } from "lucide-vue-next"
 
 interface Props {
   index: number
@@ -10,11 +11,46 @@ const props = defineProps<Props>()
 </script>
 
 <template>
-  <div>
-    <img :src="props.house.image" alt="" />
-    <div>
-      <div>{{ props.house.type }}</div>
-      <div>{{ props.house.country }}</div>
+  <div
+    class="mx-auto w-full max-w-[352px] cursor-pointer rounded-lg rounded-tl-[90px] bg-white p-5 shadow-1 transition hover:shadow-2xl"
+  >
+    <img :src="props.house.image" :alt="props.house.name" class="mb-8" />
+    <div class="mb-4 flex gap-x-2 text-sm">
+      <div class="rounded-full bg-green-500 px-3 text-white">
+        {{ props.house.type }}
+      </div>
+      <div class="rounded-full bg-violet-500 px-3 text-white">
+        {{ props.house.country }}
+      </div>
     </div>
+
+    <div class="max-w-[260px] text-lg font-semibold">
+      {{ props.house.address }}
+    </div>
+
+    <div class="my-4 flex gap-x-4">
+      <div class="flex items-center gap-1 text-gray-600">
+        <div class="text-[20px]">
+          <BedDouble />
+        </div>
+        <div>{{ props.house.bedrooms }}</div>
+      </div>
+
+      <div class="flex items-center gap-1 text-gray-600">
+        <div class="text-[20px]">
+          <Bath />
+        </div>
+        <div>{{ props.house.bathrooms }}</div>
+      </div>
+
+      <div class="flex items-center gap-1 text-gray-600">
+        <div class="text-[20px]">
+          <Scaling />
+        </div>
+        <div>{{ props.house.surface }}</div>
+      </div>
+    </div>
+
+    <Price class="mb-4" :price="props.house.price" />
   </div>
 </template>
