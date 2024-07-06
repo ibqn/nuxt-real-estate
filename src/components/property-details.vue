115e49919a6f2ca15f0a4acd05a7b409b57b5117
ibqn <evgen.ibqn@gmail.com>
2024-07-06 15:17:26 +0200

    add vee validate

--- src/components/property-details.vue
+++ src/components/property-details.vue
@@ -2,6 +2,18 @@
 import { useQuery } from "@tanstack/vue-query"
 import { getHouseById } from "@/apis/house"
 import { Bath, BedDouble, Scaling } from "lucide-vue-next"
+import { useForm } from "vee-validate"
+
+const { handleSubmit, defineField } = useForm()
+
+const [name, nameAttrs] = defineField("name")
+const [email, emailAttrs] = defineField("email")
+const [phone, phoneAttrs] = defineField("phone")
+const [message, messageAttrs] = defineField("message")
+
+const onSubmit = handleSubmit((values) => {
+  alert(JSON.stringify(values, null, 2))
+})
 
 const route = useRoute()
 const id = route.params.id as string
@@ -61,16 +73,68 @@ await suspense()
           <div>{{ house?.description }}</div>
         </div>
 
-        <div class="flex-1">
-          <div>
-            <div>
+        <div
+          class="mb-8 w-full flex-1 rounded-lg border border-gray-300 bg-white px-6 py-8"
+        >
+          <div class="mb-8 flex items-center gap-x-4">
+            <div class="size-20 rounded-full border border-gray-300 p-1">
               <img :src="house?.agent.image" :alt="house?.agent.name" />
             </div>
 
             <div>
-              <div>{{ house?.agent.name }}</div>
+              <div class="text-lg font-bold">{{ house?.agent.name }}</div>
+              <NuxtLink to="/" class="text-sm text-violet-700"
+                >View Listings</NuxtLink
+              >
             </div>
           </div>
+
+          <form @submit="onSubmit">
+            <input
+              v-model="name"
+              v-bind="nameAttrs"
+              placeholder="Name*"
+              type="text"
+              class="h-14 w-full rounded border border-gray-300 px-4 text-sm outline-none focus:border-violet-700"
+            />
+
+            <input
+              v-model="email"
+              v-bind="emailAttrs"
+              placeholder="Email*"
+              type="email"
+              class="h-14 w-full rounded border border-gray-300 px-4 text-sm outline-none focus:border-violet-700"
+            />
+
+            <input
+              v-model="phone"
+              v-bind="phoneAttrs"
+              placeholder="Phone number*"
+              type="text"
+              class="h-14 w-full rounded border border-gray-300 px-4 text-sm outline-none focus:border-violet-700"
+            />
+
+            <textarea
+              class="h-36 w-full resize-none rounded border border-gray-300 px-4 text-sm outline-none focus:border-violet-700"
+              v-model="message"
+              v-bind="messageAttrs"
+              placeholder="Message*"
+            ></textarea>
+
+            <div class="flex flex-col gap-2 md:flex-row">
+              <button
+                class="w-full rounded-lg bg-violet-700 p-4 text-sm capitalize text-white transition hover:bg-violet-800"
+                type="submit"
+              >
+                send message
+              </button>
+              <button
+                class="w-full rounded-lg border p-4 text-sm capitalize text-violet-700 transition hover:border-violet-500 hover:text-violet-500"
+              >
+                call
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
