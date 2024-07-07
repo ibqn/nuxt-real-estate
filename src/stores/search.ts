a267ad74e01bb87070b58b3ac45bdc1055fdaa74
ibqn <evgen.ibqn@gmail.com>
2024-07-07 12:31:01 +0200

    define search store

---
+++ src/stores/search.ts
@@ -0,0 +1,26 @@
+export const useSearchStore = defineStore("search", () => {
+  const defaultLocation = "Location (any)"
+  const defaultProperty = "Property (any)"
+  const defaultPriceRange = "Price range (any)"
+
+  const location = ref(defaultLocation)
+  const property = ref(defaultProperty)
+  const priceRange = ref(defaultPriceRange)
+
+  const setLocation = (newLocation: string) => (location.value = newLocation)
+  const setProperty = (newProperty: string) => (property.value = newProperty)
+  const setPriceRange = (newPriceRange: string) =>
+    (priceRange.value = newPriceRange)
+
+  return {
+    location,
+    property,
+    priceRange,
+    setLocation,
+    setProperty,
+    setPriceRange,
+    defaultLocation,
+    defaultProperty,
+    defaultPriceRange,
+  }
+})
