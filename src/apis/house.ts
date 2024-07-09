7604d24763a7ff55c653ca85fef3d3a3961102f2
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-07-09 08:50:08 +0200

    simulate random delay

--- src/apis/house.ts
+++ src/apis/house.ts
@@ -1,5 +1,10 @@
+function sleep(time: number) {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
+
 export const getHouses = async (queryString: string) => {
   const houses = await $fetch(`/api/houses?${queryString}`)
+  await sleep(Math.floor(Math.random() * 2000))
   return houses
 }
 
