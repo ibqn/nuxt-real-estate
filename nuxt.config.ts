3c7e37f4394c39c52c5fd41290a8433e8bd77904
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-07-14 17:33:27 +0200

    Update nuxt.config.ts with compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,12 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: "src/",
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "@pinia/nuxt"],
+
   googleFonts: {
     families: {
       Poppins: true,
     },
   },
+
+  compatibilityDate: "2024-07-14",
 })
